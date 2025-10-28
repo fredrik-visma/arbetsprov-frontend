6e9bc3002f7446d589fc9ba3ad8958460c80615e
felicia.muller <felicia.muller@visma.com>
2025-10-28 13:12:22 +0100

    Updated javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,33 @@
-(function(WGR, $) {
-	'use strict';
-	
-	// Set variables
-	var $input = $('.js-search-input'),
-		$products = $('.js-product');
-	
-	// Bind events
-	$input.on('input', filterProducts);
+(function(WGR) {
+    'use strict';
 
-	function filterProducts()
-	{
-		var filterValue = $input.val();
+    // Set variables
+    var input = document.querySelector('.js-search-input'),
+        products = document.querySelectorAll('.js-product');
 
-		$products.each(function(index) {
-			var title = $(this).data('title');
+    // Bind events
+    input.addEventListener('input', filterProducts);
 
-			if(title.includes(filterValue)){
-				$(this).addClass('is-visible');
-				$(this).removeClass('is-hidden');
-			}
-			else {
-				$(this).addClass('is-hidden');
-				$(this).removeClass('is-visible');
-			}
-		});
-	}
+    function filterProducts() {
+        var filterValue = input.value;
 
-	function example()
-	{
-		console.log('Hello world');
-	}
+        products.forEach(function(product) {
+            var title = product.getAttribute('data-title');
 
-	WGR.example = example;
+            if (title.includes(filterValue)) {
+                product.classList.add('is-visible');
+                product.classList.remove('is-hidden');
+            } else {
+                product.classList.add('is-hidden');
+                product.classList.remove('is-visible');
+            }
+        });
+    }
 
-}(window.WGR = window.WGR || {}, jQuery));
+    function example() {
+        console.log('Hello world');
+    }
+
+    WGR.example = example;
+
+}(window.WGR = window.WGR || {}));
